94555366498620d1d84c83f6f4b94bf155124558
Skeledan <121405337+TheRealSkeledan@users.noreply.github.com>
2024-12-01 16:44:30 -0500

    Added the characters

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+import java.io.IOException;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Main extends JPanel {
+    public static void main(String[] args) throws IOException, InterruptedException, UnsupportedAudioFileException, LineUnavailableException {
+		JFrame frame = new JFrame("SSF");
+        
+		frame.setSize(1280, 720);
+		frame.setLocation(0, 0);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setContentPane(new Main());
+		frame.setVisible(true);
     }
 }
