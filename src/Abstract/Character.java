91ba24c5c32a03732a9240e13d8fe75309a65be3
Skeledan <121405337+TheRealSkeledan@users.noreply.github.com>
2024-12-04 10:40:38 -0500

    Update Character.java

--- src/Abstract/Character.java
+++ src/Abstract/Character.java
@@ -9,33 +9,10 @@ public abstract class Entity {
 	protected double speed;
 	protected BufferedImage image;
 
-	public Entity(String name, int hp, int dmg, double speed, double X, double Y) {
-		x = X;
-		y = Y;
+	public Character(String name, int hp, int dmg, double speed) {
 		this.name = name;
 		this.hp = hp;
 		this.dmg = dmg;
 		this.speed = speed;
 	}
-
-	public void move() {
-		x += 1;
-		y += 1;
-	}
-
-	public double getX() {
-		return x;
-	}
-
-	public double getY() {
-		return y;
-	}
-
-	public BufferedImage getImage(){
-		return image;
-	}
-
-	public abstract void attackPatterns();
-
-	public abstract void sound();
 }
